1713413cd56be7b75305816a00408b45f178c5b8
Arch-Unique <ikennaidigo@gmail.com>
2021-12-22 10:18:26 +0100

    Add splashscreen constants

---
+++ lib/swiftvote.dart
@@ -0,0 +1,16 @@
+// This is a static class that contains constants and the likes.
+
+import 'package:flutter/material.dart';
+
+class SwiftVote {
+  //General
+  static const primaryColor = Color(0XFF003478);
+
+  //Splashscreen --- ss
+  static const ssprogressFill = Color(0XFF407BFF);
+  static const ssprogressDefault = Color(0XFFAAC4FF);
+  static const sstextColor = Color(0XFF22272E);
+  static const ssbuttonColor = Color(0XFFCBDBFF);
+
+  //For other screens put them below here
+}
